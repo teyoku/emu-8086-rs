a3eda1f1469c64b7013d8a26d5559f754cf42699
teyoku <teyoku.dev@gmail.com>
2026-09-01 00:24:11 +0500

    reorganize module imports and enhance main function structure

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-pub mod instructions;
 pub mod cpu;
-pub mod registers;
+pub mod errors;
+pub mod instructions;
 pub mod memory;
+pub mod registers;
+
 use std::{error::Error, fs};
 
 use clap::Parser;
+
+use crate::{cpu::Cpu, instructions::decode, memory::Memory};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -17,7 +21,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     let input_bytes = fs::read(&args.file_path)?;
 
-    println!("Bytes: {input_bytes:?}");
+    let mut memory = Memory::new();
+    let mut cpu = Cpu::default();
+
+
+    // load program into memory
+    for (i, byte) in input_bytes.iter().enumerate() {
+        memory.write_byte(0x0000 + i, *byte)?;
+    }
+
+    loop {
+        let opcode = cpu.fetch_byte(&memory)?;
+        let instruction = decode(opcode, &mut cpu, &memory)?;
+        let should_stop = cpu.execute(instruction, &mut memory)?;
+
+        if should_stop {
+            break;
+        }
+    }
 
     Ok(())
 }
