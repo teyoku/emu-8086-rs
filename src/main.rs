5d343a17ffe905860b75147b25f074920e6f70e3
teyoku <teyoku.dev@gmail.com>
2026-08-31 19:01:37 +0500

    implement command-line argument parsing for file input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs};
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    file_path: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    let input_bytes = fs::read_to_string(&args.file_path)?.into_bytes();
+
+    println!("Bytes: {input_bytes:?}");
+
+    Ok(())
 }
